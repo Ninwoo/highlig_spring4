cda86e08c5fdde466e36a11e17a110071611a570
joliu <ljo0412@live.comm>
2018-09-11 19:35:20 +0800

    Spring-Boot学习代码_ch2 Scope学习代码,测试了默认Scope：singleton和Prototype的特点
    * Singleton :唯一，全容器共享
    * Prototype :每次调用新建一个Bean实例

---
+++ src/main/java/com/typh/highlight_spring4/ch2/scope/DemoSingletonService.java
@@ -0,0 +1,7 @@
+package com.typh.highlight_spring4.ch2.scope;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoSingletonService {
+}
---
+++ src/main/java/com/typh/highlight_spring4/ch2/scope/ScopeConfig.java
@@ -0,0 +1,9 @@
+package com.typh.highlight_spring4.ch2.scope;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.typh.highlight_spring4.ch2.scope")
+public class ScopeConfig {
+}
